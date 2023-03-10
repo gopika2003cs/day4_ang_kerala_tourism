72cf016182bfb591a86a9f2558273ff595982c9f
gopika2003cs <126462082+gopika2003cs@users.noreply.github.com>
2023-03-10 20:14:58 +0530

    Add files via upload

---
+++ src/assets/data.ts
@@ -0,0 +1,78 @@
+export const district = [
+    {
+        name : "Thiruvananthapuram",
+        img : "https://youimg1.tripcdn.com/target/10081f000001gqhjy23BC_C_640_320_R5_Q70.jpg_.webp?proc=source%2Ftrip",
+        content:"Padmanabhaswamy Temple: This ancient temple, located in the heart of the city, is dedicated to Lord Vishnu and is known for its intricate architecture and exquisite sculptures. Kovalam Beach: This beautiful beach, located just a few kilometers from the city center, is known for its golden sand, crystal clear waters, and breathtaking sunsets. Napier Museum: This museum, located in the heart of the city, houses a vast collection of artifacts and exhibits related to the history and culture of Kerala.Kuthira Malika: This palace, located near the Padmanabhaswamy Temple, is known for its intricate wood carvings and traditional architecture.Thiruvananthapuram Zoo: This zoo, located in the heart of the city, is home to a wide variety of animals, birds, and reptiles.Vizhinjam Lighthouse: This historic lighthouse, located in the fishing village of Vizhinjam, offers stunning views of the Arabian Sea and the surrounding coastline. Neyyar Wildlife Sanctuary: This wildlife sanctuary, located on the outskirts of the city, is home to a wide variety of animals, birds, and reptiles, including tigers, elephants, and sambar deer.",
+    },
+    {
+        name : "Kollam",
+        content:"Kollam, also known as Quilon, is a coastal city located in the southern Indian state of Kerala. It is situated on the banks of the Ashtamudi Lake, which is a major backwater tourism destination in Kerala.Kollam is known for its rich history, cultural heritage, and natural beauty. ",
+        img : "https://assets.traveltriangle.com/blog/wp-content/uploads/2019/08/cover-for-Things-to-do-in-Kollam.jpg"
+    },
+    {
+        name : "Pathanamthitta",
+        content:"Pathanamthitta is a small town located in the southern Indian state of Kerala. It is situated in the central part of the state, and is surrounded by the Western Ghats on one side and the paddy fields of Kuttanad on the other.Pathanamthitta is known for its natural beauty and cultural heritage",
+        img : "https://www.honeymoontourskerala.com/wp-content/uploads/2016/09/p1-1.gif"
+    },
+    {
+        name : "Alappuzha",
+        content:"Alappuzha, also known as Alleppey, is a town located in the southern Indian state of Kerala. It is known for its picturesque backwaters, network of canals, and beautiful beaches.Alappuzha is also known for its vibrant culture, traditional cuisine, and colorful festivals. The town is well-connected by road and is a popular destination for tourists from all over the world, especially those who want to experience the beauty and charm of Kerala's backwaters.",
+        img : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Alappuzha_Boat_Beauty_W.jpg/1200px-Alappuzha_Boat_Beauty_W.jpg"
+    },
+    {
+        name : "Kottayam",
+        content:"Kottayam is a town located in the southern Indian state of Kerala. It is situated in the central part of the state and is known for its scenic beauty, rich history, and cultural heritage. Kottayam is also known for its vibrant culture, traditional cuisine, and colorful festivals. The town is well-connected by road and rail and is a popular destination for tourists from all over the world, especially those who want to experience the beauty and charm of Kerala's rich cultural heritage.",
+        img : "https://mediaim.expedia.com/destination/2/2fb608b6ca5306d789f0cedb81228541.jpg"
+    },
+    {
+        name : "Idukki",
+        content:"Idukki is known for its beautiful hill stations, wildlife sanctuaries, and stunning natural beauty. The district is home to the famous Munnar hill station, which is known for its sprawling tea estates and breathtaking views.",
+        img : "https://keralatourism.travel/images/destinations/places-to-visit/idukki/munnar-idukki-kerala-tourism-entry-fee-timings-holidays-reviews-small.jpg"
+    },
+    {
+        name : "Ernakulam",
+        content:" Ernakulam is the commercial capital of Kerala and is home to a number of industries, including tourism, information technology, and shipping. The district is also known for its beautiful beaches, historic landmarks, and vibrant culture.",
+        img : "https://upload.wikimedia.org/wikipedia/commons/8/8f/Kochi_Skyline.jpg"
+    },
+    {
+        name : "Thrissur",
+        content:"Known as the Cultural Capital of Kerala, Thrissur is home to a number of famous festivals and cultural events, including the Thrissur Pooram festival. The district is also known for its beautiful temples, museums, and art galleries.",
+        img : "https://img.traveltriangle.com/blog/wp-content/uploads/2018/03/cover6.jpg"
+    },
+    {
+        name : "Malappuram",
+        content:"Malappuram is known for its rich cultural heritage, beautiful mosques, and stunning natural beauty. The district is also home to the famous Kadalundi Bird Sanctuary, which is a popular destination for birdwatchers.",
+        img : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/4b/a4/d9/b612-20170815-155320.jpg?w=700&h=500&s=1"
+    },
+    {
+        name : "Palakkad",
+        content:"Palakkad is a town located in the southern Indian state of Kerala. It is situated in the northern part of the state, bordering the neighboring state of Tamil Nadu. Palakkad is known for its scenic beauty, rich cultural heritage, and historical significance.",
+        img : "https://www.oyorooms.com/travel-guide/wp-content/uploads/2021/05/Palakkad-1-1.jpg"
+    },
+    {
+        name : "Kozhikode",
+        content:"Kozhikode, also known as Calicut, is known for its beautiful beaches, historic landmarks, and vibrant culture. The district is home to the famous Kappad Beach, which is where Portuguese explorer Vasco da Gama first landed in India. Other must-visit attractions in the district include the Beypore Beach, Kozhikode Backwaters, and the Thusharagiri Waterfalls.",
+        img : "https://images.indianexpress.com/2021/07/DSC_0031.jpg"
+    },
+    {
+        name : "Wayanad",
+        content:"Wayanad is known for its stunning natural beauty, with lush green forests, rolling hills, and beautiful waterfalls. The district is also home to a number of wildlife sanctuaries, including the Wayanad Wildlife Sanctuary, which is home to elephants, tigers, and leopards.",
+        img : "https://en-media.thebetterindia.com/uploads/2019/01/Wayanad-1.jpg"
+    },
+    {
+        name : "Kannur",
+        content:"Kannur is known for its beautiful beaches, historic landmarks, and vibrant culture. The district is home to the famous St. Angelo Fort, which was built by the Portuguese in the 16th century. Other must-visit attractions in the district include the Payyambalam Beach, Muzhappilangad Beach, and the Parassinikkadavu Snake Park.",
+        img : "https://th-i.thgim.com/public/news/national/kerala/83tkmt/article25883708.ece/alternates/FREE_1200/02tvkr-palakkayamthattu"
+    },
+    {
+        name : "Kasaragod",
+        content:"Kasaragod is known for its beautiful beaches, historic landmarks, and rich cultural heritage. The district is home to the famous Bekal Fort, which is one of the largest forts in Kerala. Other must-visit attractions in the district include the Bekal Beach, Ananthapura Lake Temple, and the Chandragiri Fort.",
+        img : "https://www.keralatourism.org/images/destination/mobile/aquatourism_in_kasaragod20131107114603_306_2.jpg"
+    },
+
+
+
+
+
+]
+
---
+++ src/assets/hotsopt.ts
@@ -0,0 +1,18 @@
+export const hotspots = [
+    {
+        name:"Aralam Wildlife Sanctuary",
+        img:"https://www.hillstationboss.com/wp-content/uploads/2016/07/aralam-wildlife.jpg",
+        msg:"Is among the finest of its kind in Kerala.It plays host to an exotic array of flora and fauna,"
+    },
+    {
+        name:"Thusharagiri Waterfalls",
+        img:"https://yometro.com/images/places/thusharagiri-falls.jpg",
+        msg:"Thusharagiri or 'mist-capped peaks' gets its name from the beautiful silvery crown formed at the mountain top",
+    },
+    {
+        name:"Koyikkal Palace",
+        img:"https://www.makemytrip.com/travel-guide/media/dg_image/thiruvananthapuram/Koyikkal-Palace-Thiruvananthapuram.jpg",
+        msg:"The Koyikkal Palace was built for Umayamma Rani of the Venad Royal Family between 1677 and 1684.",
+    }
+
+]
